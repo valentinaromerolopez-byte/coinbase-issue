61dba63ede953be38b4706c8541a725763ea3b16
ga-reth <hi@gareth-v.com>
2025-09-12 16:07:22 +0100

    init

---
+++ src/wagmi.ts
@@ -0,0 +1,21 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+} from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+  ],
+  ssr: true,
+});
